f8c0663c05efe6e28ab7abc8f11c39b55c36d63c
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-12-14 08:02:10 +0100

    Add DB, update properties

--- src/main/java/com/github/azalurg/zoomanager/ZooManagerApplication.java
+++ src/main/java/com/github/azalurg/zoomanager/ZooManagerApplication.java
@@ -1,7 +1,9 @@
 package com.github.azalurg.zoomanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ZooManagerApplication {
@@ -10,4 +12,9 @@ public class ZooManagerApplication {
 		SpringApplication.run(ZooManagerApplication.class, args);
 	}
 
+  @Bean
+	public CommandLineRunner setUpApp() {
+		return (args) -> {
+		};
+	}
 }
