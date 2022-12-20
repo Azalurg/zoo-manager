a7b2b08e29713c0a618a7308dc81e7651a04c91f
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-12-20 22:55:20 +0100

    Update/Create repositories

---
+++ src/main/java/com/github/azalurg/zoomanager/repositories/KeeperRepository.java
@@ -0,0 +1,7 @@
+package com.github.azalurg.zoomanager.repositories;
+
+import com.github.azalurg.zoomanager.models.Keeper;
+import org.springframework.data.repository.CrudRepository;
+
+public interface KeeperRepository extends CrudRepository<Keeper, Long> {
+}
