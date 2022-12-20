d6dc015efcdbfa97e7aeadfa899475c2bc17be9c
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-12-20 22:55:55 +0100

    Update/Create services

--- src/main/java/com/github/azalurg/zoomanager/services/AnimalService.java
+++ src/main/java/com/github/azalurg/zoomanager/services/AnimalService.java
@@ -1,44 +1,49 @@
 package com.github.azalurg.zoomanager.services;
 
+import com.github.azalurg.zoomanager.api.ResourceNotFoundException;
 import com.github.azalurg.zoomanager.models.Animal;
+import com.github.azalurg.zoomanager.models.Keeper;
 import com.github.azalurg.zoomanager.repositories.AnimalRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
 @Transactional
 public class AnimalService {
-    final AnimalRepository animalRepository;
+
+    private final AnimalRepository animalRepository;
 
     public AnimalService(AnimalRepository animalRepository) {
         this.animalRepository = animalRepository;
     }
 
-    public Animal addAnimal(Animal animal) {
+    public List<Animal> getAllAnimals() {
+        return (List<Animal>) animalRepository.findAll();
+    }
+
+    public Animal getAnimalById(Long id) {
+        return animalRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Animal not found"));
+    }
+
+    public Animal createAnimal(Animal animal) {
         return animalRepository.save(animal);
     }
 
-    public Iterable<Animal> findAllAnimals(){
-        return animalRepository.findAll();
+    public Animal updateAnimal(Long id, Animal animal) {
+        Animal existingAnimal = getAnimalById(id);
+        existingAnimal.setName(animal.getName());
+        existingAnimal.setDescription(animal.getDescription());
+        return animalRepository.save(existingAnimal);
     }
 
-    public void test() throws ParseException {
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Animal animal1 = new Animal("Sloniu", dateFormat.parse("19/05/1956"));
-        Animal animal2 = new Animal("Strusiu", dateFormat.parse("13/11/1957"));
-        Animal animal3 = new Animal("Żółwiu", dateFormat.parse("02/01/2004"));
+    public void deleteAnimal(Long id) {
+        animalRepository.deleteById(id);
+    }
 
-        this.animalRepository.save(animal1);
-        this.animalRepository.save(animal2);
-        this.animalRepository.save(animal3);
-
-        System.out.println(this.animalRepository.findAll());
+    public void addKeeperToAnimal(Animal animal, Keeper keeper) {
+        animal.addKeeper(keeper);
+        animalRepository.save(animal);
     }
 }
---
+++ src/main/java/com/github/azalurg/zoomanager/services/KeeperService.java
@@ -0,0 +1,57 @@
+package com.github.azalurg.zoomanager.services;
+
+import com.github.azalurg.zoomanager.api.ResourceNotFoundException;
+import com.github.azalurg.zoomanager.models.Animal;
+import com.github.azalurg.zoomanager.models.Keeper;
+import com.github.azalurg.zoomanager.repositories.KeeperRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class KeeperService {
+
+    private final KeeperRepository keeperRepository;
+
+    public KeeperService(KeeperRepository keeperRepository) {
+        this.keeperRepository = keeperRepository;
+    }
+
+    public List<Keeper> getAllKeepers() {
+        return (List<Keeper>) keeperRepository.findAll();
+    }
+
+    public Keeper getKeeperById(Long id) {
+        return keeperRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Keeper not found"));
+    }
+
+    public Keeper createKeeper(Keeper keeper) {
+        return keeperRepository.save(keeper);
+    }
+
+    public Keeper updateKeeper(Long id, Keeper keeper) {
+        Keeper existingKeeper = getKeeperById(id);
+        existingKeeper.setName(keeper.getName());
+        existingKeeper.setSurname(keeper.getSurname());
+        existingKeeper.setAddress(keeper.getAddress());
+        existingKeeper.setPhone(keeper.getPhone());
+        existingKeeper.setEmail(keeper.getEmail());
+        existingKeeper.setPassword(keeper.getPassword());
+        existingKeeper.setUsername(keeper.getUsername());
+        return keeperRepository.save(existingKeeper);
+    }
+
+    public void deleteKeeper(Long id) {
+        keeperRepository.deleteById(id);
+    }
+
+    public void addAnimalToKeeper(Keeper keeper, Animal animal) {
+        if (keeper.getAnimals().contains(animal)) {
+            return;
+        }
+        keeper.addAnimal(animal);
+        keeperRepository.save(keeper);
+    }
+}
