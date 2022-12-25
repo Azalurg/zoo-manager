58a9223061935a5ea1a55ddcba6df704549abcad
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-12-25 18:48:09 +0100

    Fix loginpage

--- src/main/java/com/github/azalurg/zoomanager/web/WebKeeperController.java
+++ src/main/java/com/github/azalurg/zoomanager/web/WebKeeperController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/keepers")
 public class WebKeeperController {
@@ -18,23 +20,25 @@ public class WebKeeperController {
     private KeeperService keeperService;
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("error", "");
         return "keepers/login";
     }
 
-//    @GetMapping("/profile")
-//    public String profile(Model model) {
-//        // get logged-in keeper
-//        Keeper keeper = ...;
-//        model.addAttribute("keeper", keeper);
-//        return "keepers/profile";
-//    }
+    @GetMapping("/profile")
+    public String profile(Model model, HttpSession session) {
+        // get logged-in keeper from the session attribute
+        Keeper keeper = (Keeper) session.getAttribute("loggedInUser");
+        model.addAttribute("keeper", keeper);
+        return "keepers/profile";
+    }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+    public String login(@RequestParam String username, @RequestParam String password, Model model, HttpSession session) {
         Keeper keeper = keeperService.findByUsername(username);
         if (keeper != null && password.equals(keeper.getPassword())) {
-            // login successful
+            // login successful, store the logged-in keeper in a session attribute
+            session.setAttribute("loggedInUser", keeper);
             return "redirect:/keepers/profile";
         } else {
             // login failed
